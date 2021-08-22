b809f387275361b8a736b986228f9a2fd950586a
SeeDashMusic <85347510+SHS666638@users.noreply.github.com>
2021-08-22 14:14:40 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+   let scope = import_env_scope()
+   let play_vc = import_commingsoon_scope() /*Comming Soon*/
+  check.on():
+   let online_txt = optfine_txt_welcomelog()
+ let prefix_start = import_env_prefix_txt_official()
